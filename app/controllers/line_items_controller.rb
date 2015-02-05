eb00b60b015a4eb0c77e39852cfa9524430e183c
Biotin <qayibxanov@gmail.com>
2015-02-05 18:21:19 +0100

    AddSomeStuff

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -57,9 +57,10 @@ class LineItemsController < ApplicationController
   # DELETE /line_items/1
   # DELETE /line_items/1.json
   def destroy
+    @line_item = LineItem.find(params[:id])
     @line_item.destroy
     respond_to do |format|
-      format.html { redirect_to line_items_url, notice: 'Line item was successfully destroyed.' }
+      format.html { redirect_to (@line_item.cart), notice: 'Product was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
