8f1d7feb2ccaf542475781a8c948ca0ed6e00399
Biotin <qayibxanov@gmail.com>
2015-02-05 17:17:24 +0100

    superCart3

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -54,11 +54,12 @@ class CartsController < ApplicationController
   # DELETE /carts/1
   # DELETE /carts/1.json
   def destroy
-    @cart.destroy
-    respond_to do |format|
-      format.html { redirect_to carts_url, notice: 'Cart was successfully destroyed.' }
+    @cart.destroy if @cart.id == session[:cart_id]
+     session[:cart_id] = nil
+     respond_to do |format|
+      format.html { redirect_to store_url, notice: 'Now you cart is empty!' }
       format.json { head :no_content }
-    end
+     end
   end
 
   private
