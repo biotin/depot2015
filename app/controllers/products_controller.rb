3f2df8b1a60296d1f87ea90935a5e6faebbeda58
Biotin <qayibxanov@gmail.com>
2015-02-18 16:27:02 +0100

    AddingUsers

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < ApplicationController
+  before_filter :authenticate_user!, except: [:index, :show]
   before_action :set_product, only: [:show, :edit, :update, :destroy]
 
   # GET /products
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,9 @@
 class StoreController < ApplicationController
+  before_filter :authenticate_user!, except: [:index, :show]
   def index
 	@products = Product.order(:title)
+  end
+  
+  def show
   end
 end
