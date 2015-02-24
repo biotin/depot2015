debf086d04177b37b041785fb4681e874ab15c40
Biotin <qayibxanov@gmail.com>
2015-02-24 15:21:28 +0100

    addingOrderAndErrosTr

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -38,7 +38,7 @@ class OrdersController < ApplicationController
       if @order.save
 	Cart.destroy(session[:cart_id])
 	session[:cart_id] = nil
-        format.html { redirect_to store_url , notice: 'Thank you. Order successfully created' }
+        format.html { redirect_to store_url , notice: I18n.t('.thanks') }
         format.json { render action: 'show', status: :created, location: @order }
       else
         format.html { render :new }
