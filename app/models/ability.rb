63f3bc9017b150372f7e89a9eab5b142a94d75fa
Biotin <qayibxanov@gmail.com>
2015-02-26 22:45:14 +0100

    addingShowTrans

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,19 +3,24 @@ class Ability
 
   def initialize(admin_user)
 
+    admin_user ||= AdminUser.new
+
+
     if admin_user.role == "productmanager"
       can :manage, Product
       can :read, ActiveAdmin::Page, :name => "Dashboard"
-    end
 
-    if admin_user.role == "ordermanager"
+    elseif admin_user.role == "ordermanager"
       can :manage, Order
       can :read, ActiveAdmin::Page, :name => "Dashboard"
-    end
 
-    if admin_user.role == 'administrator'
+    elseif admin_user.role == 'administrator'
       can :manage, :all
       can :read, ActiveAdmin::Page, :name => "Dashboard"
+
+    else
+      can :read, Product
     end
+    
  end
 end
